4f2baf8aa9c9c2b06d8522e31baec7f964f77a28
Nilesh Sandeep Singh <nileshsomvansi@gmail.com>
2024-01-24 12:32:39 +0530

    removed redux-persist import

--- src/entry-client.jsx
+++ src/entry-client.jsx
@@ -4,16 +4,13 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Routes } from "./Router/Routes";
 import { Provider } from "react-redux";
-import store, { persistor } from "./store";
-import { PersistGate } from "redux-persist/integration/react";
+import store from "./store";
 const BrowserRouter = createBrowserRouter(Routes);
 ReactDOM.hydrateRoot(
   document.getElementById("root"),
   <React.StrictMode>
     <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
       <RouterProvider router={BrowserRouter} />
-    </PersistGate>
     </Provider>
   </React.StrictMode>
 );
